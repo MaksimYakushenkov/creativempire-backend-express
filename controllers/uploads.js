c6f7985c61a6043bfdaaacab8f69af13d61cfbfb
Maksim Yakushenkov <breakingwebnews@icloud.com>
2022-10-26 21:43:17 +0300

    feat: Wrote API

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -12,6 +12,6 @@ module.exports.uploadFile = (req, res) => {
       console.log(err);
       return res.status(500).send('err');
     }
-    return res.send({ url: `http://localhost:3000/uploads/${file.name}` });
+    return res.send({ url: `http://api.creativempire.ru/uploads/${file.name}` });
   });
 };
