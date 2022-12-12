d50399a3c9d543a0031317f574327da88e03a172
Maksim Yakushenkov <breakingwebnews@icloud.com>
2022-12-12 17:21:27 +0300

    feat: CORS Rules

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const fileUpload = require('express-fileupload');
 const { limiter } = require('./utils/limiter/limiter');
 const { dataMovies, PORT } = require('./utils/config/database');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsRules } = require('./cors');
+// const { corsRules } = require('./cors');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-//   next();
-// });
+  next();
+});
 app.use('/uploads', express.static('uploads'));
 app.use(fileUpload());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(corsRules);
+// app.use(corsRules);
 
 // подключаемся к серверу mongo
 mongoose.connect(dataMovies, {
