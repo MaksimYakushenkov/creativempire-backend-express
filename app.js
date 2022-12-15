ad875daa5225c093d69456dd6c066cc58d4501dd
Maksim Yakushenkov <breakingwebnews@icloud.com>
2022-12-15 06:46:53 +0300

    fix: CORS Rules

--- app.js
+++ app.js
@@ -7,40 +7,41 @@ const fileUpload = require('express-fileupload');
 const { limiter } = require('./utils/limiter/limiter');
 const { dataMovies, PORT } = require('./utils/config/database');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const { corsRules } = require('./cors');
+const { corsRules } = require('./cors');
 
-const allowedCors = [
-  'localhost:3000',
-  'http://localhost:3000',
-  'https://localhost:3000',
-  'localhost:3001',
-  'http://localhost:3001',
-  'https://localhost:3001',
-  'http://creativempire.ru',
-  'https://creativempire.ru',
-  'https://www.creativempire.ru',
-  'http://www.creativempire.ru',
-  'creativempire.ru',
-  'http://localhost:3000/',
-];
+// const allowedCors = [
+//   'localhost:3000',
+//   'http://localhost:3000',
+//   'https://localhost:3000',
+//   'localhost:3001',
+//   'http://localhost:3001',
+//   'https://localhost:3001',
+//   'http://creativempire.ru',
+//   'https://creativempire.ru',
+//   'https://www.creativempire.ru',
+//   'http://www.creativempire.ru',
+//   'creativempire.ru',
+//   'http://localhost:3000/',
+// ];
 
 const app = express();
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, PATCH');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+// app.use((req, res, next) => {
+//   const { origin } = req.headers;
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   res.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, PATCH');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
-  next();
-});
+//   next();
+// });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(corsRules);
 app.use('/uploads', express.static('uploads'));
 app.use(fileUpload());
 app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(corsRules);
 
 // подключаемся к серверу mongo
 mongoose.connect(dataMovies, {
--- cors.js
+++ cors.js
@@ -14,11 +14,35 @@ const allowedCors = [
   'http://localhost:3000/',
 ];
 
+// const corsRules = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
+//   const requestHeaders = req.headers['x-csrf-token'];
+
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Request-Headers', 'x-csrf-token');
+//     return res.end();
+//   }
+
+//   return next();
+// };
+
+// module.exports = {
+//   corsRules,
+// };
 const corsRules = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
-  const requestHeaders = req.headers['x-csrf-token'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -27,8 +51,6 @@ const corsRules = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Request-Headers', 'x-csrf-token');
     return res.end();
   }
 
